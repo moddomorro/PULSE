dc128ea566d2c978063b28a6dbbc80d3f34e3e16
moddomorro <sergey.yavon2701@gmail.com>
2022-08-22 13:47:52 +0300

    new blocks, features

--- src/js/script.js
+++ src/js/script.js
@@ -38,4 +38,55 @@ $(document).ready(function(){
     toggleSlide('.catalog-item__back');
 
 
+
+    //modal
+
+    $('[data-modal=consultation]').on('click', function(){
+      $('.overlay, #consultation').fadeIn('0.5s');
+    });
+    $('.modal__close').on('click', function(){
+      $('.overlay, #consultation, #thanks, #order').fadeOut('0.5s')
+    });
+
+
+    $('.button_mini').each(function(i) {
+      $(this).on('click', function() {
+        $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+        $('.overlay, #order').fadeIn('0.5s');
+      })
+    });
+
+    function validateForms(form){
+      $(form).validate({
+          rules: {
+              name: {
+                  required: true,
+                  minlength: 2
+              },
+              phone: "required",
+              email: {
+                  required: true,
+                  email: true
+              }
+          },
+          messages: {
+              name: {
+                  required: "Пожалуйста, введите свое имя",
+                  minlength: jQuery.validator.format("Введите {0} символа!")
+                },
+              phone: "Пожалуйста, введите свой номер телефона",
+              email: {
+                required: "Пожалуйста, введите свою почту",
+                email: "Неправильно введен адрес почты"
+              }
+          }
+      });
+  };
+
+  validateForms('#consultation-form');
+  validateForms('#consultation form');
+  validateForms('#order form');
+
+
+
 });
